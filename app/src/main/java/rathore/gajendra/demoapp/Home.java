326dda24723c872c2eac1637f47f04673146d5c3
Avinash_Singh <avi13smartest@gmail.com>
2017-02-06 22:06:36 +0530

    search view

--- app/src/main/java/rathore/gajendra/demoapp/Home.java
+++ app/src/main/java/rathore/gajendra/demoapp/Home.java
@@ -51,7 +51,7 @@ public class Home extends AppCompatActivity  {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Mr.Shoes", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
